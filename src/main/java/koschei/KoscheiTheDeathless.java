b07a9c57af2c32a69aba1b371f90d74d1020876a
Айрат Латыпов <ayrat.latypov.2020@bk.ru>
2024-04-19 02:29:00 +0300

    build

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,5 +1,6 @@
 package koschei;
 
+import koschei.models.Duck5;
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,13 +9,19 @@ import org.springframework.stereotype.Component;
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
+    private Duck5 duck5;
 
     public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
+        return "На свете есть океан , " + ocean.toString() + duck5.toString();
     }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
+    }
+
+    @Autowired
+    public void setDuck5(Duck5 duck5) {
+        this.duck5 = duck5;
     }
 }
